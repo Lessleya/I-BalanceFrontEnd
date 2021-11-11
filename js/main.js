48386631fbae362e6f73b4cd70d4209e190eee29
eddyog <gon19005@byui.edu>
2021-11-11 11:04:37 -0700

    task

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function categories(category){
+
+    switch(category) {
+        case 'spiritual':
+            
+            document.getElementById("logo").src="../images/spiritual-logo.png";
+
+          break;
+        case 'intelectual':
+
+          break;
+
+        case 'social':
+
+        break;
+
+        case'physical':
+
+        break;
+
+        default:
+          // code block
+      }
+
+}
