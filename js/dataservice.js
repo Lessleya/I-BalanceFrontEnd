9d45b49b3389096605323c9a1e10a20be30df98a
sebastiansteve <43971058+sebastiansteve@users.noreply.github.com>
2021-11-18 10:59:06 -0700

    we are getting the csrf token yeah

--- js/dataservice.js
+++ js/dataservice.js
@@ -2,7 +2,12 @@ const url = 'https://cse341-ibalance-api.herokuapp.com/'
 class DataService
 {
     static async getCsrfToken(){
-        return await fetch(url + "login").then(res=>{res.json(); console.log(res.json())})
+        return await fetch(url + "login").then(async res=>
+            {
+                res = await res.json(); 
+                console.log(res); 
+                return res._csrf
+            })
     }
     static async postLogin(data){
         const option = {method:"post", Headers:{"Content-Type":"multipart/form-data"}, body:data}
--- js/login.js
+++ js/login.js
@@ -1,6 +1,5 @@
 document.querySelector(".login-form").addEventListener("submit", processLogin)
-function processLogin(){
-    console.log('help')
-    const data ={email:document.getElementById('email'), password:document.getElementById('password'), _csrf:DataService.getCsrfToken()}
-    DataService.postLogin(data).then(res=> console.log(res))
+async function processLogin(){
+    const data = await {email:document.getElementById('email'), password:document.getElementById('password'), _csrf: await DataService.getCsrfToken()}
+    await DataService.postLogin(data).then(async res=> console.log(await res))
 }
