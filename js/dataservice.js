a8b40c6572224b7f14116700594448d0afe7195d
sebastiansteve <43971058+sebastiansteve@users.noreply.github.com>
2021-11-18 11:20:46 -0700

    almost there

--- js/dataservice.js
+++ js/dataservice.js
@@ -4,13 +4,12 @@ class DataService
     static async getCsrfToken(){
         return await fetch(url + "login").then(async res=>
             {
-                res = await res.json(); 
-                console.log(res); 
+                res = await res.json();  
                 return res._csrf
             })
     }
     static async postLogin(data){
-        const option = {method:"post", Headers:{"Content-Type":"multipart/form-data"}, body:data}
+        const option = {method:"post", credentials:"include", Headers:{"Content-Type":"application/json"}, body:JSON.stringify(data)}
         return await fetch(url + "login", option).then(res=> res.json())
     }
 }
--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 document.querySelector(".login-form").addEventListener("submit", processLogin)
 async function processLogin(){
-    const data = await {email:document.getElementById('email'), password:document.getElementById('password'), _csrf: await DataService.getCsrfToken()}
+    const _csrf = await DataService.getCsrfToken()
+    const data = {email:document.getElementById('emaillogin').value, password:document.getElementById('passwordlogin').value, _csrf: _csrf}
     await DataService.postLogin(data).then(async res=> console.log(await res))
 }
