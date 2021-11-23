9bb912889f90b25c6b7100217929fba0919cef41
sebastiansteve <43971058+sebastiansteve@users.noreply.github.com>
2021-11-23 11:12:44 -0700

    fix connection issue

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 document.querySelector(".login-form").addEventListener("submit", processLogin)
-async function processLogin(){
+async function processLogin(e){
+    e.preventDefault();
     const _csrf = await DataService.getCsrfToken()
     const data = {email:document.getElementById('emaillogin').value, password:document.getElementById('passwordlogin').value, _csrf: _csrf}
     await DataService.postLogin(data).then(async res=> console.log(await res))
