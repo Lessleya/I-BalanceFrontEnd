5204cc0db08a06688ba37e8a85d98026f9ae18ce
mebenal <willtreatyaraluen@gmail.com>
2021-11-27 15:17:01 -0700

    fixed talking to backend

--- js/dataservice.js
+++ js/dataservice.js
@@ -1,15 +1,19 @@
 const url = 'https://cse341-ibalance-api.herokuapp.com/'
+//const url = 'http://localhost:3000/'
+
 class DataService
 {
-    static async getCsrfToken(){
-        return await fetch(url + "login").then(async res=>
-            {
-                res = await res.json();  
+    static async getCsrfToken() {
+            const option = { method: "get", credentials: 'include', headers: { "Content-Type": "application/json" } }
+            return await fetch(url + "login", option).then(async res=>
+        {
+            res = await res.json();
+            console.log(res)
                 return res._csrf
             })
     }
-    static async postLogin(data){
-        const option = {method:"post", credentials:"include", Headers:{"Content-Type":"application/json"}, body:JSON.stringify(data)}
+    static async postLogin(data) {
+        const option = {method:"post", credentials: 'include', headers:{"Content-Type":"application/json"}, body:JSON.stringify(data)}
         return await fetch(url + "login", option).then(res=> res.json())
     }
 }
