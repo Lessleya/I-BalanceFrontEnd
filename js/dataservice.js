7abbf6bb6fcd285d74844f8420bc3edc189704df
mebenal <willtreatyaraluen@gmail.com>
2021-11-27 16:51:06 -0700

    fixed talking to backend

--- js/dataservice.js
+++ js/dataservice.js
@@ -5,15 +5,15 @@ class DataService
 {
     static async getCsrfToken() {
             const option = { method: "get", credentials: 'include', headers: { "Content-Type": "application/json" } }
-            return await fetch(url + "login", option).then(async res=>
+            return await fetch(url, option).then(async res=>
         {
             res = await res.json();
             console.log(res)
                 return res._csrf
             })
     }
-    static async postLogin(data) {
+    static async post(data, endpoint) {
         const option = {method:"post", credentials: 'include', headers:{"Content-Type":"application/json"}, body:JSON.stringify(data)}
-        return await fetch(url + "login", option).then(res=> res.json())
+        return await fetch(url + endpoint, option).then(res=> res.json())
     }
 }
