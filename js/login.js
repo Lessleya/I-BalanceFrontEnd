bcb7be18b545e0e703124cc12aded4de17729f3f
sebastiansteve <43971058+sebastiansteve@users.noreply.github.com>
2021-11-30 10:38:50 -0700

    registor form

--- js/login.js
+++ js/login.js
@@ -12,3 +12,26 @@ async function processLogin(e) {
     console.log(await res)
   );
 }
+document.querySelector('.register-form').addEventListener('submit', processRegister);
+async function processRegister(e) {
+  e.preventDefault();
+  const _csrf = await DataService.getCsrfToken();
+  fpassword = document.getElementById('password').value
+  spassword = document.getElementById('verifyPassword').value
+  if (fpassword != spassword){
+   let error = document.getElementsByClassName('hidden-error')[0]
+   error.value = "Password don't match"
+   error.style.display
+  }
+  const data = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    password: fpassword,
+    confirmPassword: spassword,
+    _csrf: _csrf,
+  };
+  await DataService.post(data, 'signup');
+  await DataService.get('daily-tasks').then(async res =>
+    console.log(await res)
+  );
+}
