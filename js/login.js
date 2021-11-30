6d3d221580fb7dc4578024e229f103d8e0b47752
mebenal <willtreatyaraluen@gmail.com>
2021-11-30 07:52:30 -0700

    ready for merge

--- js/login.js
+++ js/login.js
@@ -1,7 +1,14 @@
-document.querySelector(".login-form").addEventListener("submit", processLogin)
-async function processLogin(e){
-    e.preventDefault();
-    const _csrf = await DataService.getCsrfToken()
-    const data = {email:document.getElementById('emaillogin').value, password:document.getElementById('passwordlogin').value, _csrf: _csrf}
-    await DataService.postLogin(data).then(async res=> console.log(await res))
+document.querySelector('.login-form').addEventListener('submit', processLogin);
+async function processLogin(e) {
+  e.preventDefault();
+  const _csrf = await DataService.getCsrfToken();
+  const data = {
+    email: document.getElementById('emaillogin').value,
+    password: document.getElementById('passwordlogin').value,
+    _csrf: _csrf,
+  };
+  await DataService.post(data, 'login');
+  await DataService.get('daily-tasks').then(async res =>
+    console.log(await res)
+  );
 }
