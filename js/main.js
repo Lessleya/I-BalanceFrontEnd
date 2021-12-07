18295c3eaf06f13da5e792ebff87692eaca5f2d6
eddyog <gon19005@byui.edu>
2021-12-07 10:02:50 -0700

    update colors

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@
 function categories(category){
 
   var title_element = document.getElementById("categoryTitle");
-  var backgroundColor_spiritual="#e3c9dd"; 
-  var backgroundColor_intelecctual = "#dfffd6";
-  var backgroundColor_social = "#c4d3d4";
-  var backgroundColor_physical ="#f3a87c ";
+  var backgroundColor_spiritual="#FFDA92"; 
+  var backgroundColor_intelecctual = "#9EE0D8";
+  var backgroundColor_social = "#BBE09E";
+  var backgroundColor_physical ="#A1E7FD";
 
   // var logo = document.getElementsById("category-logo").;
 
@@ -16,12 +16,14 @@ function categories(category){
 
           title_element.innerHTML="Spiritual";
           document.getElementById("category-logo").src="../images/spiritual-logo.png";
+          document.getElementById("category-image").src="../images/spiritual-image.png";
           document.getElementById("hero-today").style.backgroundColor=backgroundColor_spiritual;
 
           break;
         case 'intelectual':
           title_element.innerHTML="Intelectual";
           document.getElementById("category-logo").src="../images/intelectual-logo.png";
+          document.getElementById("category-image").src="../images/intelectual-image.png";
           document.getElementById("hero-today").style.backgroundColor=backgroundColor_intelecctual;
 
           break;
@@ -29,6 +31,7 @@ function categories(category){
         case 'social':
           title_element.innerHTML="Social";
           document.getElementById("category-logo").src="../images/social-logo.png";
+          document.getElementById("category-image").src="../images/social-image.png";
           document.getElementById("hero-today").style.backgroundColor=backgroundColor_social;
 
         break;
@@ -36,6 +39,7 @@ function categories(category){
         case'physical':
         title_element.innerHTML="Physical";
         document.getElementById("category-logo").src="../images/physical-logo.png";
+        document.getElementById("category-image").src="../images/physical-image.png";
         document.getElementById("hero-today").style.backgroundColor=backgroundColor_physical;
 
         break;
