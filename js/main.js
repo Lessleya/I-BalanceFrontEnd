f6d9baddef722bd55dd8e64222f423700f36c344
Lesliee Cruz <71569547+Lessleya@users.noreply.github.com>
2021-12-09 08:48:53 -0700

    updated

--- js/main.js
+++ js/main.js
@@ -78,5 +78,23 @@ if (menu) {
         menu.classList.toggle("is-active");
         menuLinks.classList.toggle("active");
     });
-    
+}
+
+window.onscroll = function() {
+    myFunction();
+};
+
+// Get the header
+var header = document.getElementById("main-header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
 }
