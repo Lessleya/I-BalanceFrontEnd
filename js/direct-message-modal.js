cb064f3d922f11a791f4b77914b89e0be6162598
mebenal <willtreatyaraluen@gmail.com>
2021-12-08 22:57:56 -0700

    working sockets

--- js/direct-message-modal.js
+++ js/direct-message-modal.js
@@ -1,53 +1,68 @@
-import isLoggedIn from './check-login.js'
-const socket = io("https://cse341-ibalance-api.herokuapp.com/", {transports: ['websocket']});
+import isLoggedIn from './check-login.js';
+const socket = io('https://cse341-ibalance-api.herokuapp.com/', {
+  transports: ['websocket'],
+});
 
 isLoggedIn().then(email => {
-    console.log(email)
-    socket.emit('name', email)    
-})
+  console.log(email);
+  socket.emit('name', email);
+  socket.email = email
+});
 
 socket.emit('news', 'hello');
 
-socket.on('ack', (data) => {
-    console.log('here')
-    console.log(data);
-    console.log('here2')
-    if (data.success)
-    {
-        console.log('click is working')
-        document.getElementById('myBtn').addEventListener("click", loadDirectMessageContent)
-        console.log('click enabled')
+socket.on('ack', data => {
+  console.log(data);
+  console.log(socket);
+  if (data.success) {
+    console.log('click is working');
+    document
+      .getElementById('myBtn')
+      .addEventListener('click', loadDirectMessageContent);
+    console.log('click enabled');
+  }
+});
+
+function loadDirectMessageContent() {
+  console.log('here3');
+  socket.emit(
+    'messageTo',
+    document.getElementById('directMessageEmailInput').value
+  );
+  console.log('here4');
+  socket.on('messageData', function (data) {
+    console.log('recieve data');
+    console.log(data); //should output 'hello world'
+
+    // if(data['connected'] == false){
+    if (data['success'] == false) {
+      alert('no user found');
+      // dmc.innerHTML = "<p>No user was found</p>"
+    } else {
+      console.log('user found');
+      messageUserByEmail();
+      let dmc = document.getElementById('directMessageContent');
+      let content = "";
+      data.data.forEach(element => {
+        content += `<p class="${socket.email == element.fromUserEmail ? "sender" : "reciever"}">${element.message}</p>`
+      });
+      dmc.innerHTML = content;
+      document.getElementById('sendMessageId').addEventListener('click', sendMessage);
     }
-})
-
-
-function loadDirectMessageContent(){
-    console.log('here3')
-    socket.emit('messageTo', document.getElementById('directMessageEmailInput').value);
-    console.log('here4')
-    socket.on('messageData', function(data){
-        console.log('recieve data')
-        console.log(data);   //should output 'hello world'
-        let dmc = document.getElementById('directMessageContent');
-        
-        // if(data['connected'] == false){
-            if(data['success'] == false){
-                alert('no user found')
-                // dmc.innerHTML = "<p>No user was found</p>"
-            }
-        else{
-            console.log("user found")
-            messageUserByEmail();
-            dmc.innerHTML = data;
-        }
-    
-    });
+  });
 }
 
-function messageUserByEmail(){
-    // e.preventDefault();
-    let modal = document.getElementById('myModal');
-    modal.innerHTML = `<div class="modal-content">
+socket.on('recieveMessage', data => {
+  console.log(data)
+  let dmc = document.getElementById('directMessageContent');
+  const toAdd = `<p class="${socket.email == data.fromUserEmail ? "sender" : "reciever"}">${data.message}</p>`
+  dmc.innerHTML += toAdd
+})
+
+function messageUserByEmail() {
+  // e.preventDefault();
+  let modal = document.getElementById('myModal');
+  modal.innerHTML = `<div class="modal-content">
     <div class="modal-header">
     <span class="close">&times;</span>
     <h2>Message friend</h2>
@@ -63,39 +78,40 @@ function messageUserByEmail(){
         <form id="directMessageForm">
         <textarea name="directMessageFormInput" id="directMessageFormInput" placeholder="Message..." height: 36px></textarea>
         </form>
-        <button class="btn" id="sendMessageId" onclick=sendMessage >Send</button>
+        <button class="btn" id="sendMessageId">Send</button>
     </div>
     
     </div>
     
-</div>`
+</div>`;
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks on the button, open the modal
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName('close')[0];
+  // When the user clicks on the button, open the modal
 
-modal.style.display = "block";
+  modal.style.display = 'block';
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-if (event.target == modal) {
-modal.style.display = "none";
-}
-}
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = 'none';
+  };
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  };
 }
 
 // Modal section
 
-function sendMessage(){
-    let message = document.getElementById('directMessageFormInput');
-    let time = new Date.now()
-    let toEmail = document.getElementById('directMessageEmailInput').value;
-    socket.emit('sendMessage', {'message': message, 'date': time, 'toEmail': email});
-
+function sendMessage() {
+  let message = document.getElementById('directMessageFormInput').value;
+  let time = Date.now();
+  let toEmail = document.getElementById('directMessageEmailInput').value;
+  socket.emit('sendMessage', {
+    message: message,
+    date: time,
+    toEmail: toEmail,
+  });
 }
-
-
